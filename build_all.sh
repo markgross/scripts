437dfff6f93bbda4360be46ce6b2f0005203b5f0
mark gross <markgross@thegnar.org>
2012-11-22 06:29:15 -0800

    clean up script so I can see the build times and a quick check that the vmlinux files get created.

--- build_all.sh
+++ build_all.sh
@@ -3,9 +3,9 @@
 top=`pwd`
 ssd="/mnt/ssd"
 
-QUEUE="git://git.kernel.org/pub/scm/linux/kernel/git/stable/stable-queue.git"
-STABLE="git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git"
-MIRROR="$ssd$top/mirror/linux-stable.git"
+#QUEUE="git://git.kernel.org/pub/scm/linux/kernel/git/stable/stable-queue.git"
+#STABLE="git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux-stable.git"
+#MIRROR="$ssd$top/mirror/linux-stable.git"
 
 cd stable-queue
 git pull
@@ -16,17 +16,18 @@ export PATH="/usr/lib/ccache:$PATH"
 
 for b in $trees; do mkdir $top/$b; cd $top/$b; \
        cd linux-stable; \
-       git clean -xdf; \
-       git merge --abort; \
-       git am --abort; \
-       git rebase --abort; \
-       git reset --hard; \
+       git clean -xdf 2>&1 > /dev/null; \
+       git merge --abort 2>&1 > /dev/null; \
+       git am --abort 2>&1 > /dev/null; \
+       git rebase --abort 2>&1 > /dev/null; \
+       git reset --hard 2>&1 > /dev/null; \
        branch=${b#"queue-"}; \
        git checkout origin/linux-$branch.y  -b `date -u +%Y_%b_%d_%H_%M_UTC`; \
        cp -r $top/stable-queue/$b patches; \
-       quilt push -a; \
+       nohup quilt push -a; \
        source $top/use_ccache.sh; \
        nohup make defconfig; \
        time nohup make -j 8; \
+       file vmlinux; \
        done;
 
